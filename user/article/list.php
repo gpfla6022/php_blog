43d44180ded02fa159b1ef4a608b52e241e337f2
gpfla6022 <gpfla3503@gmail.com>
2021-05-23 02:11:57 +0900

    DB결과 압축푸는 반복문 코드줄임

--- user/article/list.php
+++ user/article/list.php
@@ -10,13 +10,7 @@ $rs = mysqli_query($dbConn, $sql);
 
 $articles = [];
 
-while (true) {
-    $article = mysqli_fetch_assoc($rs);
-
-    if($article == null) {
-        break;
-    }
-
+while ($article = mysqli_fetch_assoc($rs)) {
     $articles[] = $article;
 }
 
